f854747086729e94c75f1cce4ee71917faf3c895
RipuRipu1 <theworrkkers@gmail.com>
2024-12-07 20:03:20 +0700

    add react to javascript

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const app = document.getElementById('O2'); // select an element by ID
+const main = document.getElementById('O2'); // select an element by ID
 
 const element = document.createElement('h1'); // make new element
 
@@ -9,3 +9,10 @@ const textContent = document.createTextNode(text);
 element.appendChild(textContent); // append aka place the text into the h1 element we just created
 
 app.appendChild(element); // place the h1 element we just created inside the div
+
+// target html element
+const app = document.getElementById('react-test')
+const root = ReactDOM.createRoot(app);
+
+// render the element
+root.render(<h1>Hello World</h1>)
